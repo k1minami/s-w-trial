98094a212f097056c9a6197cde5ac79c4f93cc9c
k1minami <minami@project-arx.com>
2019-03-06 18:21:27 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,39 +1,20 @@
-/**
- * Here is is the code snippet to initialize Firebase Messaging in the Service
- * Worker when your app is not hosted on Firebase Hosting.
- // [START initialize_firebase_in_sw]
- // Give the service worker access to Firebase Messaging.
- // Note that you can only use Firebase Messaging here, other Firebase libraries
- // are not available in the service worker.
  importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-app.js');
  importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-messaging.js');
  
- // Initialize the Firebase app in the service worker by passing in the
- // messagingSenderId.
  firebase.initializeApp({
    'messagingSenderId': '419458027222'
  });
- // Retrieve an instance of Firebase Messaging so that it can handle background
- // messages.
- const messaging = firebase.messaging();
- // [END initialize_firebase_in_sw]
- **/
 
-
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// [START background_handler]
+const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  var notificationTitle = 'Moutakuto Notice';
+  var notificationTitle = payload.notification.title;
   var notificationOptions = {
-    body: 'Background Moutakuto Message.',
+    body: payload.notification.body,
     icon: '/img/moutakuto.png'
   };
 
   return self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
-// [END background_handler]
