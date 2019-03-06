efe6c1132754647e573d75578281cffe15c44b1f
k1minami <minami@project-arx.com>
2019-03-06 18:09:18 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,13 +5,13 @@
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here, other Firebase libraries
  // are not available in the service worker.
- importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/5.8.5/firebase-messaging.js');
  
  // Initialize the Firebase app in the service worker by passing in the
  // messagingSenderId.
  firebase.initializeApp({
-   'messagingSenderId': 'YOUR-SENDER-ID'
+   'messagingSenderId': '419458027222'
  });
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
@@ -30,7 +30,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
   var notificationTitle = 'Background Message Title';
   var notificationOptions = {
     body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    icon: '/img/moutakuto.png'
   };
 
   return self.registration.showNotification(notificationTitle,
